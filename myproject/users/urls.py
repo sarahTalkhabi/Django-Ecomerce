1114f093db178ecfe9b77f2e068a2645fd0fae7a
sarahTalkhabi <124080804+sarahTalkhabi@users.noreply.github.com>
2023-02-07 11:38:00 +0000

    Add files via upload

---
+++ myproject/users/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path 
+from . import views
+from django.contrib.auth import views as authentication_view
+app_name='users'
+
+urlpatterns = [
+    path('register/',views.register,name='register'),
+    path('login/',authentication_view.LoginView.as_view(template_name='users/login.html'),name='login'),
+    path('logout/',authentication_view.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('profile/',views.profile,name='profile'),
+    path('createprofile/',views.create_profile, name='createprofile'),
+    path('sellerprofile/<int:id>/',views.seller_profile, name='sellerprofile'),
+
+
+]
